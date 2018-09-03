d7943d00bc2cda64379cd34651bbd0521795fa44
Pete Muir <pmuir@bleepbleep.org.uk>
2018-09-03 11:40:18 +0100

    Remove max-reconnect, doesn't make sense in kube

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var fileSystem = require('fs');
 const crd = require('./pipelineactivities-crd.json');
 const moment = require('moment');
 
-const maxReconnect = 10;
-
 async function main() {
 	try {
 		const Client = require('kubernetes-client').Client;
@@ -21,7 +19,7 @@ async function main() {
 		//
 		// Get a JSON stream for pipelineactivities events and send it to the http response
 		//
-		watch(client, 0, null);
+		watch(client, null);
 
 	} catch (err) {
 		console.error('Error: ', err);
@@ -34,14 +32,13 @@ async function main() {
  * Kubernetes will disconnect the stream periodically, so we reconnect on the end event.
  * By default Kubernetes will return the stream history each time we connect, so to avoid this
  * we only display new events by tracking the creationTimestamp of the event, and only displaying
- * newer events. After 10 reconnections we exit.
+ * newer events.
  * 
  * @param {*} client the shared client we use to watch the stream
- * @param {*} reconnectCount the number of times we have reconnected
  * @param {*} creationTimestamp the last creationTimestamp we observed, or null if we haven't seen 
  * 								one yet
  */
-function watch(client, reconnectCount, creationTimestamp) {
+function watch(client, creationTimestamp) {
 	// create a stream against the watch endpoint
 	const stream = client.apis['jenkins.io'].v1.watch.pipelineactivities.getStream();
 	const jsonStream = new JSONStream();
@@ -51,21 +48,15 @@ function watch(client, reconnectCount, creationTimestamp) {
 		ct = moment(object.object.metadata.creationTimestamp);
 		creationTimestamp = creationTimestamp ? creationTimestamp : ct;
 		// Only log if this is a more recent event
-		if (ct > creationTimestamp)  {
+		if (ct > creationTimestamp) {
 			creationTimestamp = ct;
 			console.log('New Event at', creationTimestamp.format());
 			console.log(JSON.stringify(object, null, 2));
 		}
 	});
 	jsonStream.on('end', object => {
-		if (reconnectCount < maxReconnect) {
-			reconnectCount++;
-			console.log('Reconnect stream (', reconnectCount, ')')
-			watch(client, reconnectCount, creationTimestamp);
-		} else {
-			console.log('Reconnected', maxReconnect, 'times, exiting');
-			process.exit();
-		}
+		console.log('Reconnect stream');
+		watch(client, creationTimestamp);
 	});
 }
 
